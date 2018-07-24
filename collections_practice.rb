dd625a24d093108960e20ed4d492868c76094230
Justin Wang <justinw827@aol.com>
2018-07-24 22:50:02 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,50 @@
-# your code goes here
+def begins_with_r(array)
+  array.each do |e|
+    if e[0] != 'r' && e[0] != 'R'
+      return false
+    end
+  end
+  return true
+end
+
+def contain_a(array)
+  myArray = []
+  array.each do |e|
+    if e.include? 'a'
+      myArray.push(e)
+    end
+  end
+  return myArray
+end
+
+def first_wa(array)
+  array.each do |e|
+    if e[0] == 'w' && e[1] == 'a'
+      return e
+    end
+  end
+end
+
+def remove_non_strings(array)
+  array.delete_if { |e| !(e.is_a? String) }
+end
+
+def count_elements(array)
+  array.each_with_object(Hash.new(0)) {  }
+end
+
+def merge_data(keys, data)
+  myArray = []
+  keys.each_with_index do |e, i|
+    myArray << e << data
+  end
+  myArray
+end
+
+def find_cool(hashes)
+  
+end
+
+def organize_schools(schools)
+
+end
